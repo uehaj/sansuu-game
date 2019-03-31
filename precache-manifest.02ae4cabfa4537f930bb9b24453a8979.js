6bc3f7c1af4de5b29d898abeac59d1b1d8ebd60a
UEHARA, Junji <59012+uehaj@users.noreply.github.com>
2019-03-31 16:22:12 +0900

    Updates

---
+++ precache-manifest.02ae4cabfa4537f930bb9b24453a8979.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8bdf0f6785562a5999",
+    "url": "/sansuu-game/static/js/main.104b8820.chunk.js"
+  },
+  {
+    "revision": "086f532a03a9c04981bb",
+    "url": "/sansuu-game/static/js/runtime~main.030a4066.js"
+  },
+  {
+    "revision": "452aa40603bc242d3ac1",
+    "url": "/sansuu-game/static/js/2.aba2729c.chunk.js"
+  },
+  {
+    "revision": "5de2dfe60d1a7ae82295176cc0bdacdb",
+    "url": "/sansuu-game/index.html"
+  }
+];
