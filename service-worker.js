f44ec86843c6300e7508517dfc1167526d24c46d
UEHARA, Junji <59012+uehaj@users.noreply.github.com>
2019-03-31 15:42:21 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "http://uehaj.github.io/sansuu-game/precache-manifest.cbc418ebee1455e53a515657d94caca4.js"
+  "./precache-manifest.ec8028e18da996c0b2355906e01e0356.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("http://uehaj.github.io/sansuu-game/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
