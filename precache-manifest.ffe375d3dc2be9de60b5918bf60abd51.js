761d90bbcce03722e27079ec2c5b4b006bb26db4
UEHARA, Junji <59012+uehaj@users.noreply.github.com>
2019-06-06 16:23:30 +0900

    Updates

---
+++ precache-manifest.ffe375d3dc2be9de60b5918bf60abd51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b9e03f2a26d3cf1f245",
+    "url": "/sansuu-game/static/js/main.7d250a0c.chunk.js"
+  },
+  {
+    "revision": "086f532a03a9c04981bb",
+    "url": "/sansuu-game/static/js/runtime~main.030a4066.js"
+  },
+  {
+    "revision": "24dbe9126530c717ed00",
+    "url": "/sansuu-game/static/js/2.9a7bc424.chunk.js"
+  },
+  {
+    "revision": "54a447b0d25fa8822fe13cb54f7c50e0",
+    "url": "/sansuu-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bed9f7ed3e694e68fb597d7fbdf14e3f.js"
+  "/sansuu-game/precache-manifest.ffe375d3dc2be9de60b5918bf60abd51.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/sansuu-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
